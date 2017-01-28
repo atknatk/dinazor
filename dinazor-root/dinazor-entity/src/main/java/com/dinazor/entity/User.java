28077a4627dc7a08199f402679e1497fe16c6f18
Atakan Atik <atknatk@gmail.com>
2017-01-28 11:52:20 +0200

    First Commit

---
+++ dinazor-root/dinazor-entity/src/main/java/com/dinazor/entity/User.java
@@ -0,0 +1,13 @@
+package com.dinazor.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+/**
+ * Created by Administrator on 25.01.2017.
+ */
+
+@Entity
+@Table(name="person")
+public class User {
+}
